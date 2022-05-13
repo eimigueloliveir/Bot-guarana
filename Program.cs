167c40a3a32500087574b13c03d7ec97864c4902
Miguel Oliveira <Miguel Oliveira@REVISION-PC>
2022-05-13 17:31:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Tweetinvi;
+using Tweetinvi.Models.V2;
+
+string[] config = File.ReadAllLines("/home/ubuntu/github/config/bot-guarana.config");
+//Line 0 of file contains CONSUMER_KEY
+//Line 1 of file contains CONSUMER_SECRET
+//Line 2 of file contains ACCESS_TOKEN
+//Line 3 of file contains ACCESS_TOKEN_SECRET
+TwitterClient client = new(config[0], config[1], config[2], config[3]);
+Console.WriteLine("Starting...");
+while (true)
+{
+    try
+    {
+        SearchTweetsV2Response search = await client.SearchV2.SearchTweetsAsync("guarana");
+        TweetV2 tweet = search.Tweets.First();
+        if (tweet.Text.Contains("guarana") || tweet.Text.Contains("guaraná"))
+        {
+            await client.Tweets.PublishRetweetAsync(long.Parse(tweet.Id));
+            Console.WriteLine($"{tweet.Id} Retweeted");
+            await client.Tweets.FavoriteTweetAsync(long.Parse(tweet.Id));
+            Console.WriteLine($"{tweet.Id} Favorited");
+            Thread.Sleep(36000);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}");
+    }
+}
